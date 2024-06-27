d4033a0e6ec3a50e4799ca7c7c8ad06104ce07e0
Iriagamb <77029483+Iriagamb@users.noreply.github.com>
2024-06-27 00:37:29 +0000

    home

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,148 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
+
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	
+		const [inputByUser, setInputByUser] = useState("");
+		const [tasks, setTasks] = useState([]);
+		const apiUrl = "https://playground.4geeks.com/todo";
+	  
+		const pressEvent = (event) => {
+		  if (event.key == "Enter") {
+			if (event.target.value == "") {
+			  return alert("Enter your task!");
+			}
+			addNewTask(inputByUser)
+			setInputByUser("");
+			
+		  }
+		};
+	  
+		const taskAdder = (event) => {
+		  setInputByUser(event.target.value);
+		};
+	  
+		
+		const createUser = async () => { 
+			try {
+				const response = await fetch(`${apiUrl}/users/Iriagamb`, {method: "POST"}) 
+				if (!response.ok){
+					throw new Error ("No se pudo crear el usuario")
+				}
+				const data = await response.json() 
+				console.log(data)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+
+		const getList = async () => { 
+			try {
+				const response = await fetch(`${apiUrl}/users/Iriagamb`) 
+				if (!response.ok){
+					throw new Error ("No se pudo obtener la lista")
+				}
+				const data = await response.json() 
+				console.log(data)
+				setTasks (data.todos)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+
+		const addNewTask = async (newTask) => { 
+			try {
+				const response = await fetch(`${apiUrl}/todos/Iriagamb`, {
+					method: "POST",
+					body: JSON.stringify(
+						{
+							"label": newTask,
+							"is_done": false
+						  }
+					),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				}) 
+				if (!response.ok){
+					throw new Error ("No se pudo agregar la tarea")
+				}
+				const data = await response.json() 
+				console.log(data) 
+				setTasks([...tasks,data])
+			} catch (error) {
+				console.log(error)
+			}
+		}
+
+		const deleteTask = async (id) => { 
+			try {
+				const response = await fetch(`${apiUrl}/todos/${id}`, {method: "DELETE"}) 
+				if (!response.ok){
+					throw new Error ("No se pudo eliminar la tarea")
+				}
+				const data = await response
+				console.log(data)
+				const newArray = tasks.filter(item => item.id != id )
+				setTasks(newArray)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+ 
+		useEffect(() => {
+			createUser()
+			getList()
+		}, [])
+
+
+
+	
+
+		return (
+		  <div className="container">
+			<h1 className="text-center">Todos</h1>
+			<div className="card  border border-4">
+			  <div className="mt-3">
+				<input
+				  type="text"
+				  className="form-control"
+				  value={inputByUser}
+				  onChange={taskAdder}
+				  placeholder="Ingresa tu tarea "
+				  onKeyDown={pressEvent}
+				/>
+			  </div>
+			  <div className="tasks-section mt-3 mx-1">
+				{tasks.length == 0 ? (
+				  <span>No hay tareas, por favor añade tareas </span>
+				) : (
+				  tasks.map((task) => (
+					<div
+					  key={task.id}
+					  className="task justify-content-between text-start container d-flex my-2"
+					>
+					  <p>{task.label}</p>
+					  <button type="button" class="btn-close" aria-label="Close"
+						onClick={() => deleteTask(task.id)}
+					  >
+						
+					  </button>
+					</div>
+				  ))
+				)}
+			  </div>
+			</div>
+			<div className="card-footer">
+			  <p>Tareas agregadas {tasks.length}</p>
+			</div>
+		  </div>
+		
 	);
-};
+  };
 
 export default Home;
